812af74049396766dccdf1bdcebcc896ff0870ad
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-05-24 20:21:05 +0900

    init commit

---
+++ SP20_simulator/src/SP20_simulator/InstLuncher.java
@@ -0,0 +1,14 @@
+package SP20_simulator;
+
+// instruction에 따라 동작을 수행하는 메소드를 정의하는 클래스
+
+public class InstLuncher {
+    ResourceManager rMgr;
+
+    public InstLuncher(ResourceManager resourceManager) {
+        this.rMgr = resourceManager;
+    }
+
+    // instruction 별로 동작을 수행하는 메소드를 정의
+    // ex) public void add(){...}
+}
---
+++ SP20_simulator/src/SP20_simulator/VisualSimulator.java
@@ -0,0 +1,50 @@
+package SP20_simulator;
+
+import java.awt.EventQueue;
+import java.io.File;
+
+/**
+ * VisualSimulator는 사용자와의 상호작용을 담당한다.<br>
+ * 즉, 버튼 클릭등의 이벤트를 전달하고 그에 따른 결과값을 화면에 업데이트 하는 역할을 수행한다.<br>
+ * 실제적인 작업은 SicSimulator에서 수행하도록 구현한다.
+ */
+public class VisualSimulator {
+	ResourceManager resourceManager = new ResourceManager();
+	SicLoader sicLoader = new SicLoader(resourceManager);
+	SicSimulator sicSimulator = new SicSimulator(resourceManager);
+	
+	/**
+	 * 프로그램 로드 명령을 전달한다.
+	 */
+	public void load(File program){
+		//...
+		sicLoader.load(program);
+		sicSimulator.load(program);
+	};
+
+	/**
+	 * 하나의 명령어만 수행할 것을 SicSimulator에 요청한다.
+	 */
+	public void oneStep(){
+		
+	};
+
+	/**
+	 * 남아있는 모든 명령어를 수행할 것을 SicSimulator에 요청한다.
+	 */
+	public void allStep(){
+		
+	};
+	
+	/**
+	 * 화면을 최신값으로 갱신하는 역할을 수행한다.
+	 */
+	public void update(){
+		
+	};
+	
+
+	public static void main(String[] args) {
+		
+	}
+}
